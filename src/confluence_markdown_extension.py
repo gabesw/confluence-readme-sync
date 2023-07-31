bbebc0cc03119af7a2c2a6a946e8ae1b575cbc22
Gabe Woloz <gwoloz@bigid.com>
2023-07-31 16:04:54 -0400

    removed language line from code block
    no syntax highlighting

--- src/confluence_markdown_extension.py
+++ src/confluence_markdown_extension.py
@@ -3,15 +3,24 @@ from markdown.postprocessors import Postprocessor
 from markdown.preprocessors import Preprocessor
 from markdown.extensions import Extension
 
-class ConfluencePreprocessor(Preprocessor):
+class SectionLinkPreprocessor(Preprocessor):
     def run(self, lines):
         modified_lines = []
         for line in lines:
             # replace links to sections on the page with one hashtag instead of multiple to work in confluence urls
             modified_lines.append(re.sub(r'\]\(#+', r'](#', line, flags=re.DOTALL))
         return modified_lines
+    
+class CodeLanguagePreprocessor(Preprocessor):
+    def run(self, lines):
+        modified_lines = []
+        for line in lines:
+            #replace language in codeblock with special language tag to be grabbed by the CodeBlockPostprocessor
+            modified_line = re.sub(r'```(\w+)', r'```', line, flags=re.DOTALL)
+            modified_lines.append(modified_line)
+        return modified_lines
 
-class ConfluencePostprocessor(Postprocessor):
+class CodeBlockPostprocessor(Postprocessor):
     def run(self, text):
         # replace code blocks with confluence code blocks
         processed_text = re.sub(
@@ -25,8 +34,9 @@ class ConfluencePostprocessor(Postprocessor):
 class ConfluenceExtension(Extension):
     def extendMarkdown(self, md):
         md.registerExtension(self)
-        md.preprocessors.register(ConfluencePreprocessor(md), 'confluence_section_links', 0)
-        md.postprocessors.register(ConfluencePostprocessor(md), 'confluence_code_block', 0)
+        md.preprocessors.register(SectionLinkPreprocessor(md), 'confluence_section_links', 0)
+        md.preprocessors.register(CodeLanguagePreprocessor(md), 'confluence_section_links', 0)
+        md.postprocessors.register(CodeBlockPostprocessor(md), 'confluence_code_block', 0)
 
 def makeExtension(*args, **kwargs):
     return ConfluenceExtension(*args, **kwargs)
